55da49a686b130fcb09810fa03c2b98c92f0e5ac
Костя Осадчий <qwrhevsjjdb83736@gmail.com>
2024-10-20 00:01:27 +0300

    Добавьте файлы проекта.

---
+++ Model/Printing Press.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Базовий клас ДрукарськеВидання
+abstract class ДрукарськеВидання
+{
+    public string Назва { get; set; }
+    public double Ціна { get; set; }
+
+    public ДрукарськеВидання(string назва, double ціна)
+    {
+        Назва = назва;
+        Ціна = ціна;
+    }
+
+    // Два віртуальні методи
+    public virtual void ПоказатиДані()
+    {
+        Console.WriteLine($"Назва: {Назва}, Ціна: {Ціна}");
+    }
+
+    public virtual void ОновитиЦіну(double новаЦіна)
+    {
+        Ціна = новаЦіна;
+        Console.WriteLine($"Ціна оновлена до: {Ціна}");
+    }
+}
